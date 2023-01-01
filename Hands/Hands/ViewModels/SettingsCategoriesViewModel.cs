9e6e81cb73f2f9f4ff883a5e8ad378e4b5b40517
Huy Nguyen <touch@nguyenrk.com>
2023-01-02 00:27:15 +0700

    Update

---
+++ Hands/Hands/ViewModels/SettingsCategoriesViewModel.cs
@@ -0,0 +1,37 @@
+﻿using Splat;
+using ReactiveUI;
+using DynamicData;
+using System;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Hands.Services;
+using Hands.Models;
+
+
+using Xamarin.Forms;
+using Xamarin.Essentials;
+using System.Web;
+
+
+namespace Hands.ViewModels
+{
+    public class SettingsAccountsViewModel : ReactiveObject
+    {
+        private readonly ISettingsService service;
+
+        public SettingsAccountsViewModel()
+        {
+            service = Locator.Current.GetService<ISettingsService>();
+
+            service.ConnectAccountsSetting()
+                .ObserveOn(RxApp.MainThreadScheduler)
+                .Bind(out items)
+                .Subscribe();
+        }
+
+        private readonly ReadOnlyObservableCollection<TAccount> items;
+        public ReadOnlyObservableCollection<TAccount> Items => items;
+    }
+}
---
+++ Hands/Hands/Views/SettingsCategoriesPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using ReactiveUI.XamForms;
+using Xamarin.Forms;
+using Hands.ViewModels;
+
+namespace Hands.Views
+{
+    public partial class SettingsAccountsPage : ReactiveContentPage<SettingsAccountsViewModel>
+    {
+        public SettingsAccountsPage()
+        {
+            InitializeComponent();
+
+            ViewModel = new SettingsAccountsViewModel();
+
+            BindingContext = ViewModel;
+        }
+    }
+}
