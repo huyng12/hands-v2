9d2f0098e8cfceadfa67f87c7859cadf44010fea
Huy Nguyen <touch@nguyenrk.com>
2023-01-10 04:23:28 +0700

    Finalize report page

--- Hands/Hands/ViewModels/StatsViewModel.cs
+++ Hands/Hands/ViewModels/StatsViewModel.cs
@@ -15,6 +15,8 @@ using DynamicData.Binding;
 using System.ComponentModel;
 using System.Reactive;
 using Newtonsoft.Json;
+using DynamicData.Kernel;
+using System.Text.RegularExpressions;
 
 namespace Hands.ViewModels
 {
@@ -85,12 +87,106 @@ namespace Hands.ViewModels
         public DateTime To { get; set; }
     }
 
+    public class TransactionWithCategory
+    {
+        public TTransaction Transaction { get; set; }
+        public TCategory Category { get; set; }
+        public string CategoryName { get; set; }
+
+        public TransactionWithCategory(
+            TTransaction transaction, Optional<TCategory> category)
+        {
+            Transaction = transaction;
+            Category = category.HasValue ? category.Value : null;
+            CategoryName = category.HasValue ? category.Value.Name : "❓ Uncategorised";
+        }
+    }
+
+    public class TransactionWithCategoryGrouping : ReactiveObject, IDisposable
+    {
+        private string categoryName;
+        public string CategoryName
+        {
+            get => categoryName;
+            set => this.RaiseAndSetIfChanged(ref categoryName, value);
+        }
+
+        private string icon;
+        public string Icon
+        {
+            get => icon;
+            set => this.RaiseAndSetIfChanged(ref icon, value);
+        }
+
+        private Int64 count;
+        public Int64 Count
+        {
+            get => count;
+            set => this.RaiseAndSetIfChanged(ref count, value);
+        }
+
+        private Int64 total;
+        public Int64 Total
+        {
+            get => total;
+            set => this.RaiseAndSetIfChanged(ref total, value);
+        }
+
+        private string formattedTotal;
+        public string FormattedTotal
+        {
+            get => formattedTotal;
+            set => this.RaiseAndSetIfChanged(ref formattedTotal, value);
+        }
+
+        public TransactionWithCategoryGrouping(IGroup<TransactionWithCategory
+            , string, string> group)
+        {
+            var fullCategoryName = group.Key;
+
+            bool isStartsWithEmoji = !Regex.IsMatch(
+                fullCategoryName[0].ToString(),
+                "[a-z]", RegexOptions.IgnoreCase);
+
+            string[] parts = fullCategoryName.Split(' ');
+
+            Icon = isStartsWithEmoji ? parts.First() : "";
+            CategoryName = isStartsWithEmoji
+                    ? String.Join(" ", parts.Skip(1))
+                    : fullCategoryName;
+
+            Func<Int64, string> formatMoney = n => String.Format(
+                    CultureInfo.GetCultureInfo("en-US"), "{0:N0}", n);
+
+            var totalDisposable = group.Cache
+                .Connect()
+                .QueryWhenChanged(q => q.Items.Sum(tx => tx.Transaction.Amount))
+                .Subscribe(total =>
+                {
+                    Total = total;
+                    FormattedTotal = formatMoney(total);
+                });
+
+            var amountDisposable = group.Cache
+                .Connect()
+                .QueryWhenChanged(q => q.Items.Count())
+                .Subscribe(count => Count = count);
+
+            _cleanUp = new CompositeDisposable(totalDisposable, amountDisposable);
+        }
+
+        private readonly IDisposable _cleanUp;
+        public void Dispose() { _cleanUp?.Dispose(); }
+    }
+
     public class StatsViewModel : ReactiveObject, IDisposable
     {
+        private readonly ISettingsService settingsService;
         private readonly ITransactionService transactionService;
 
         public StatsViewModel()
         {
+            settingsService = Locator.Current.GetService<ISettingsService>();
             transactionService = Locator.Current.GetService<ITransactionService>();
 
             Timeframe = getCurrentWeekTimeframe();
@@ -108,9 +204,13 @@ namespace Hands.ViewModels
                 {
                     var from = timeframe.From.ToString("dd/MM/yyyy");
                     var to = timeframe.To.ToString("dd/MM/yyyy");
-                    //Console.WriteLine($"{timeframe.From.ToString()} - {timeframe.To.ToString()}");
                     TimeframeIndicator = $"{from} - {to}";
                 });
+
+            var categoriesObservable = settingsService
+                .ConnectCategoriesSetting()
+                .RefCount()
+                .ObserveOn(RxApp.MainThreadScheduler);
 
             var transactionsInTimeframe = transactionService
                 .Connect()
@@ -118,7 +218,22 @@ namespace Hands.ViewModels
                 .ObserveOn(RxApp.MainThreadScheduler)
                 .Filter(filterByTimeframe);
 
-            var transactionsDisposable = transactionsInTimeframe
+            var transactionsDisposable = categoriesObservable
+                .RightJoin(
+                    transactionsInTimeframe, tx => tx.CategoryId,
+                    (category, transaction) => new TransactionWithCategory(transaction, category))
+                .ChangeKey(tx => tx.Transaction.Id)
+                .Group(tx => tx.CategoryName)
+                .Transform(group => new TransactionWithCategoryGrouping(group))
+                .ChangeKey(group => group.CategoryName)
+                .AutoRefresh()
+                .Sort(SortExpressionComparer<TransactionWithCategoryGrouping>
+                    .Descending(group => group.Total))
+                .Bind(out groupedTransactions)
+                .DisposeMany()
+                .Subscribe();
+
+            var transactionColumnsDisposable = transactionsInTimeframe
                 .Filter(tx => tx.Type == CategoryType.Expense)
                 .Group(tx => tx.CreatedAt.Date)
                 .Transform(group => new TransactionColumn(group))
@@ -134,11 +249,28 @@ namespace Hands.ViewModels
                 .DisposeMany()
                 .Subscribe();
 
+            Func<Int64, string> formatMoney = n => String.Format(
+                    CultureInfo.GetCultureInfo("en-US"), "{0:N0}", n);
+
+            var formattedTotalSpentDisposable = transactionsInTimeframe
+                .Filter(tx => tx.Type == CategoryType.Expense)
+                .QueryWhenChanged(q => q.Items.Sum(tx => tx.Amount))
+                .Select(total => formatMoney(total))
+                .Subscribe(formatted => FormattedTotalSpent = formatted);
+
+            var formattedTotalReceivedDisposable = transactionsInTimeframe
+                .Filter(tx => tx.Type == CategoryType.Income)
+                .QueryWhenChanged(q => q.Items.Sum(tx => tx.Amount))
+                .Select(total => formatMoney(total))
+                .Subscribe(formatted => FormattedTotalReceived = formatted);
+
             GoBackCommand = ReactiveCommand.Create(executeGoBackCommand);
             GoNextCommand = ReactiveCommand.Create(executeGoNextCommand);
 
-            _cleanUp = new CompositeDisposable(transactionsDisposable,
-                incomesDisposable, timeframeIndicatorDisposable);
+            _cleanUp = new CompositeDisposable(transactionColumnsDisposable,
+                incomesDisposable, timeframeIndicatorDisposable,
+                transactionsDisposable, formattedTotalSpentDisposable,
+                formattedTotalReceivedDisposable);
         }
 
         private readonly ReadOnlyObservableCollection<
@@ -151,6 +283,11 @@ namespace Hands.ViewModels
         public ReadOnlyObservableCollection<
             TransactionColumn> Incomes => incomes;
 
+        private readonly ReadOnlyObservableCollection<
+            TransactionWithCategoryGrouping> groupedTransactions;
+        public ReadOnlyObservableCollection<
+            TransactionWithCategoryGrouping> GroupedTransactions => groupedTransactions;
+
         private TTimeframe timeframe;
         public TTimeframe Timeframe
         {
@@ -158,8 +295,19 @@ namespace Hands.ViewModels
             set => this.RaiseAndSetIfChanged(ref timeframe, value);
         }
 
-        //readonly ObservableAsPropertyHelper<string> timeframeIndicator;
-        //public string TimeframeIndicator => timeframeIndicator.Value;
+        private string formattedTotalSpent;
+        public string FormattedTotalSpent
+        {
+            get => formattedTotalSpent;
+            set => this.RaiseAndSetIfChanged(ref formattedTotalSpent, value);
+        }
+
+        private string formattedTotalReceived;
+        public string FormattedTotalReceived
+        {
+            get => formattedTotalReceived;
+            set => this.RaiseAndSetIfChanged(ref formattedTotalReceived, value);
+        }
 
         private string timeframeIndicator;
         public string TimeframeIndicator
